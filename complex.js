db0f8434485adb98dd0f85f88d4d3996a81e2319
mimiq <axxo2275@gmail.com>
2022-01-03 10:57:21 +0100

    swapped components everywhere, fixed toString

--- complex.js
+++ complex.js
@@ -1,40 +1,41 @@
 class ComplexNumber {
-  constructor(imaginary, real) {
-    this.imaginary = imaginary;
+  constructor(real, imaginary) {
     this.real = real;
+    this.imaginary = imaginary;
   }
 
   add(operand) {
-    if(operand instanceof ComplexNumber) {
-      let result = new ComplexNumber(this.imaginary + operand.imaginary, this.real + operand.real);
-      return result;
-    }
+    return new ComplexNumber(this.real + operand.real, this.imaginary + operand.imaginary);
   }
 
   multiply(operand) {
-    let i = this.real * operand.imaginary + this.imaginary * operand.real;
     let r = this.real * operand.real - this.imaginary * operand.imaginary;
+    let i = this.real * operand.imaginary + this.imaginary * operand.real;
 
-    return new ComplexNumber(i, r);
+    return new ComplexNumber(r, i);
   }
 
-  toString() {
-    let i = this.imaginary;
+  toString(parentheses = false) {
     let r = this.real;
+    let i = this.imaginary;
 
     let result = '';
 
     if(r != 0 || i == 0) {
       result += r;
+    } else {
+      result += i + 'i';
     }
 
-    if(i != 0) {
+    if(i != 0 && r != 0) {
       result += i > 0 ? ' + ' : ' - ';
       if(i != 1 && i != -1) {
         result += Math.abs(i);
       }
       result += 'i';
     }
+
+    if(parentheses) return '(' + result + ')';
 
     return result;
   }
